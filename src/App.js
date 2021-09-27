63c5d082a15dc785585deba2e888250898d75d38
Gabriel Gutierez <gabriel@gabriel-inspiron-14-3467>
2021-09-27 16:21:20 -0300

    inicial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import FruitsPage from "./store/modules/fruits/action";
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <FruitsPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/modules/fruits/reducer.jsx
@@ -0,0 +1,10 @@
+const inicialState = ["Banana", "Maçã", "Morango"];
+
+const fruitsReducer = (state = inicialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default fruitsReducer;
